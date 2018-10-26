cdc3b70c96cf07b3cd11c159a5a6f4558f0a6fe6
Unknown <brown.r1992@gmail.com>
2018-10-26 17:11:20 -0400

    Continued Building Enemy State Machine
    Added Functions for State Machine
    Started Patrol State Scripting

--- Assets/Scripts/Enemy/EnemyStatePatrol.cs
+++ Assets/Scripts/Enemy/EnemyStatePatrol.cs
@@ -1,16 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using StateStuff;
+using System;
 
-public class EnemyStatePatrol : MonoBehaviour {
+public class EnemyStatePatrol : State<EnemyAI>
+{
+    private static EnemyStatePatrol _instance;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private EnemyStatePatrol()
+    {
+        if(_instance != null)
+        {
+            return;
+        }
+        _instance = this;
+    }
+
+    public static EnemyStatePatrol Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                new EnemyStatePatrol();
+            }
+
+            return _instance;
+
+        }
+    }
+
+
+
+
+
+    public override void EnterState(EnemyAI _owner)
+    {
+
+        Debug.Log("Entering Patrol State");
+        throw new NotImplementedException();
+        
+    }
+
+    public override void ExitState(EnemyAI _owner)
+    {
+
+        Debug.Log("Exiting Patrol State");
+        throw new NotImplementedException();
+    }
+
+    public override void UpdateState(EnemyAI _owner)
+    {
+        throw new NotImplementedException();
+    }
 }
--- Assets/Scripts/StateMachine.cs
+++ Assets/Scripts/StateMachine.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyStateMachine : MonoBehaviour {
+public class StateMachine : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
