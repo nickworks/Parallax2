012c20db61c2d45a3b6d8a13fdc2d2b2bd180cae
Nick <pattison.nick@gmail.com>
2018-11-01 13:54:14 -0400

    Added KillInTheVoid
    + KillInTheVoid : This class Destroys an object when it falls into the void.
    * Added KillInTheVoid to the Player prefab.

---
+++ Assets/Scripts/KillInTheVoid.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillInTheVoid : MonoBehaviour {
+
+    /// <summary>
+    /// If this object falls below this y value, it will automatically die.
+    /// </summary>
+    public float yKillThreshold = -10;
+
+    /// <summary>
+    /// Checks if the object has fallen past the yKillThreshold. If so, kill the object.
+    /// </summary>
+    void Update()
+    {
+        if (transform.position.y < yKillThreshold)
+        {
+
+            HealthAndRespawn health = GetComponent<HealthAndRespawn>();
+
+            if (health)
+            {
+                health.Die();
+            } else
+            {
+                Destroy(gameObject);
+            }
+            
+        }
+    } // ends Update()
+}
