fcb1c89d85b7af25bbd2edbc1b4b440529b509f7
Nick <pattison.nick@gmail.com>
2018-11-01 13:39:24 -0400

    Documented HealthAndRespawn and DamageDealer
    Also moved some level assets into the correct folder.

--- Assets/Scripts/HealthAndRespawn.cs
+++ Assets/Scripts/HealthAndRespawn.cs
@@ -3,24 +3,55 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
+/// <summary>
+/// This gives an object health, the ability to die, and the ability to respawn
+/// at an arbitrary spawn position.
+/// </summary>
 public class HealthAndRespawn : MonoBehaviour {
 
+    /// <summary>
+    /// The world position this object should respawn at when it dies.
+    /// </summary>
     public Vector3 spawnPosition { get; private set; }
-
+    /// <summary>
+    /// The current health of this object. 0 is dead.
+    /// </summary>
     public float hpCurrent { get; private set; }
+    /// <summary>
+    /// The maximum health of this object.
+    /// </summary>
     public float hpMax = 100;
+    /// <summary>
+    /// Whether or not the player should respawn when they die.
+    /// This overrides destroyOnDeath.
+    /// </summary>
     public bool respawnOnDeath = false;
+    /// <summary>
+    /// Whether or not to destroy this game object when it dies.
+    /// This is ignored id respawnOnDeath is true.
+    /// </summary>
     public bool destroyOnDeath = false;
-
+    /// <summary>
+    /// An event to call when this object dies.
+    /// </summary>
     public UnityEvent onDeath;
-
+    /// <summary>
+    /// Sets the initial spawn position.
+    /// </summary>
     void Start () {
         SetSpawn(transform.position);
 	}
+    /// <summary>
+    /// Sets the spawn position.
+    /// </summary>
+    /// <param name="pos"></param>
     public void SetSpawn(Vector3 pos)
     {
         spawnPosition = pos;
     }
+    /// <summary>
+    /// Respawns this object at its spawn position. This resets the health to maximum.
+    /// </summary>
 	void Respawn()
     {
         hpCurrent = hpMax;
@@ -31,11 +62,19 @@ public class HealthAndRespawn : MonoBehaviour {
             zControl.z = (int)(spawnPosition.z / LayerFixed.separation);
         }
     }
+    /// <summary>
+    /// Call this method to do damage to this object. If health falls below 0, then trigger death.
+    /// </summary>
+    /// <param name="amount">How much health to take away.</param>
     public void TakeDamage(float amount)
     {
         hpCurrent -= amount;
         if (hpCurrent <= 0) Die();
     }
+    /// <summary>
+    /// Kills this object. Depending on settings, the object will
+    /// respawn, be destroyed, or fire the onDeath event.
+    /// </summary>
     public void Die()
     {
         onDeath.Invoke();
