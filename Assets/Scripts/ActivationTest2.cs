5ea628dba757d083088571483cd159855e4ee9b8
Kyle Lowery <Loweryk1@ferris.edu>
2018-11-01 14:00:32 -0400

    Applied Recommended Changes
    +Added comments to ActivationTest2
    +Changed Hitbox Detection on the prefab PhysicsButton so that it is not able to be activated from the side.

--- Assets/Scripts/ActivationTest2.cs
+++ Assets/Scripts/ActivationTest2.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //TODO: Add Comments
-
+/// <summary>
+/// This Test makes it so when a button is activated, it moves an object (like a Door) up, then lowers it once the button is released.
+/// </summary>
 public class ActivationTest2 : MonoBehaviour {
-
+    /// <summary>
+    /// When called, this function moves the GameObject "up" two meters in the World Space.
+    /// </summary>
 	void Activate ()
     {
         transform.Translate(new Vector3(0, 2, 0), Space.World);
     }
-
+    /// <summary>
+    /// When called, this function moves the GameObject "down" two meters in the World Space.
+    /// </summary>
     void Deactivate ()
     {
         transform.Translate(new Vector3(0, -2, 0), Space.World);
