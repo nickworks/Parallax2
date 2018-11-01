d533b1389dc0caf8578f895f2144cc05873789ce
Kyle Lowery <Loweryk1@ferris.edu>
2018-11-01 13:23:38 -0400

    Added Test2
    A PhysicsButton is wired to open/close a door.

---
+++ Assets/Scripts/ActivationTest2.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivationTest2 : MonoBehaviour {
+
+	void Activate ()
+    {
+        transform.Translate(new Vector3(0, 2, 0), Space.World);
+    }
+
+    void Deactivate ()
+    {
+        transform.Translate(new Vector3(0, -2, 0), Space.World);
+    }
+}
