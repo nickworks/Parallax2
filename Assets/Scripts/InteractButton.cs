0433a18837b040e7e3ad68599944180b6ebcc4a8
Kyle Lowery <Loweryk1@ferris.edu>
2018-11-08 13:32:35 -0500

    Built up the structure for the InteractButton prefab.
    -Modifed PhysicsButtonSandbox to test out an InteractButton.

---
+++ Assets/Scripts/InteractButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This script is used to control an InteractButton object. These buttons are activated when the player is within range and they press the "Interact" button.
+/// These buttons can only activate.
+/// </summary>
+public class InteractButton : MonoBehaviour {
+
+    /// <summary>
+    /// Stores the Renderer of the button so that when activated/deactivated, it can change materials to reflect beign active/deactive.
+    /// </summary>
+    Renderer buttonRend;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
