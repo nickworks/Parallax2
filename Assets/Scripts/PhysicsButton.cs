d77fdaec5444ae1fd4f06d24c36bc5a031ecd9f6
Kyle Lowery <Loweryk1@ferris.edu>
2018-11-08 14:49:51 -0500

    Added Functionality to InteractButtons
    Stored a new InteractButton prefab that can now be activated by pressing the "Submit" buttons.

--- Assets/Scripts/PhysicsButton.cs
+++ Assets/Scripts/PhysicsButton.cs
@@ -8,12 +8,6 @@ using UnityEngine.Events;
 /// This script is meant to be used on a Trigger to signal to an object when the Trigger is activated or deactivated.
 /// </summary>
 public class PhysicsButton : MonoBehaviour {
-
-    /// <summary>
-    /// Stores a reference to the object the button is attached to.
-    /// </summary>
-    //public GameObject target;
-
     /// <summary>
     /// This UnityEvent is called when something is on top of the button.
     /// </summary>
@@ -50,7 +44,6 @@ public class PhysicsButton : MonoBehaviour {
     /// <param name="other">The collider of the object that has entered the trigger area.</param>
     private void OnTriggerEnter(Collider other)
     {
-        print("hi");
         onActivate.Invoke();
        
         if (activeMat != null)
