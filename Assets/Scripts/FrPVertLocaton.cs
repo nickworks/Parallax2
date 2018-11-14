82d81dda77bfb5083752267c29a6a563390ffa76
Unknown <djmodd@live.com>
2018-11-14 15:43:27 -0500

    added mesh getter

--- Assets/Scripts/FrPVertLocaton.cs
+++ Assets/Scripts/FrPVertLocaton.cs
@@ -5,19 +5,48 @@ using UnityEngine;
 public class FrPVertLocaton : MonoBehaviour {
 
     public Camera sceneCam;
-    Mesh mesh;
+
+    
+    /// <summary>
+    /// the locations of the front four verticies
+    /// </summary>
     Vector3[] vertLocations = new Vector3[4];
 
+    /// <summary>
+    /// we will use this variable to get the Mesh on tbis object
+    /// </summary>
+    Mesh _mesh; //C# private C# feild
 
+    /// <summary>
+    /// The color of the wireframe applied to the mesh 
+    /// </summary>
+    public Color color = Color.cyan;
 
-    // Use this for initialization
+    // Mesh 
+
+    /// <summary>
+    /// get's the mesh when mesh is refrenced
+    /// </summary>
+    public Mesh mesh {//C#  property
+
+        get {
+            if (_mesh == null) _mesh = GetComponent<MeshFilter>().mesh;// "lazy" initailisation
+            return _mesh;
+        }
+    }
+
+    /// <summary>
+    /// this function initalises this class
+    /// </summary>
     void Start()
     {
-       mesh = GetComponent<Mesh> 
+       //mesh = GetComponent<Mesh> 
     }
 
 
-	// Update is called once per frame
+	/// <summary>
+    /// this function is called every frame
+    /// </summary>
 	void Update () {
 		
 	}
