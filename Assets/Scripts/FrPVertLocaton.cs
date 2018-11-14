fe2d032d68f36ebc4d0c38517732e044e77ccf81
Unknown <djmodd@live.com>
2018-11-14 15:34:11 -0500

    Started Building Detection
    +FrPVertLocation (in scripts folder)

---
+++ Assets/Scripts/FrPVertLocaton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrPVertLocaton : MonoBehaviour {
+
+    public Camera sceneCam;
+    Mesh mesh;
+    Vector3[] vertLocations = new Vector3[4];
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+       mesh = GetComponent<Mesh> 
+    }
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
