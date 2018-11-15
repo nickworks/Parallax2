b774f703c5633c5d56731db4c20ee12737352287
Jessi <petroej1@ferris.edu>
2018-11-15 11:09:03 -0500

    Jump Pads Auto jump user
    -Bug with gravity

--- Assets/Scripts/JumpPad.cs
+++ Assets/Scripts/JumpPad.cs
@@ -1,16 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>
+/// Creates a surface that autojumps the player
+/// </summary>
 public class JumpPad : MonoBehaviour {
-
-    public float bounceForce;
-
- 
-void OnCollisionEnter(Collision hit) {
-        if (hit.gameObject.tag == "Player")
-        {
-            hit.rigidbody.AddForce(bounceForce * Vector3.up, ForceMode.VelocityChange);
-        }
-    }
+	/// <summary>
+	/// The amount of force behing the jump. Currently set in engine
+	/// </summary>
+	public float amountOfForce;
+	/// <summary>
+	/// Raises the trigger enter event.
+	/// </summary>
+	/// <param name="other">The other object the player collides with</param>
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.CompareTag("Jumpable")){			
+			Vector3 playerPos = this.transform.position;
+			playerPos.y += amountOfForce;
+			transform.position = playerPos;
+			print (transform.position.y);
+			}
+	}
 }
