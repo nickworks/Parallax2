8341923b9a4e9c34097eeb6c28f256d468c242df
Jessica <petroej1@ferris.edu>
2018-11-29 13:11:30 -0500

    Added Comments

--- Assets/Scripts/JumpPad.cs
+++ Assets/Scripts/JumpPad.cs
@@ -6,6 +6,9 @@ using UnityEngine;
 /// Creates a surface that autojumps the player
 /// </summary>
 public class JumpPad : MonoBehaviour {
+    /// <summary>
+    /// Reference to the player controller
+    /// </summary>
     private PlayerController playerController;
     /// <summary>
     /// The amount of force behing the jump. Currently set in engine
@@ -19,6 +22,10 @@ public class JumpPad : MonoBehaviour {
     {
         playerController = GetComponent<PlayerController>();
     }
+    /// <summary>
+    /// When collision happens, and the tag is jumpable, the player auto jumps
+    /// </summary>
+    /// <param name="other"></param>
     void OnTriggerEnter(Collider other){
 
 		if(other.gameObject.CompareTag("Jumpable")){			
