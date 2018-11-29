1c368674e26f0c1745e82d01a33555f626e2abef
Jonah <kettmaj@ferris.edu>
2018-11-29 16:26:54 -0500

    Improved level, Redid MovingObjects
    Took class critique and changed certain things about the level, refactored parts of MovingObjects, Objects can now rotate, movable objects now work

--- Assets/Scripts/Level/MovingObject.cs
+++ Assets/Scripts/Level/MovingObject.cs
@@ -1,45 +1,82 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>
+/// this script allows objects to move between two Vector3 locations, and also rotate along their local axis'
+/// </summary>
 public class MovingObject : MonoBehaviour {
-
+    /// <summary>
+    /// if this object is for spinning
+    /// </summary>
+    public bool isSpinner;
+    /// <summary>
+    /// if this object is for moving between twho points
+    /// </summary>
+    public bool isTransform;
+    /// <summary>
+    /// tracking if this object is moving from its original position (true) or back from the 2nd postition (false)
+    /// </summary>
+    private bool fromFirst = true;
+    /// <summary>
+    /// the starting point of the object
+    /// </summary>
+    private Vector3 pointA;
+    /// <summary>
+    /// the ending point of the object
+    /// </summary>
     public Vector3 pointB;
-    public Vector3 incritement;
+    /// <summary>
+    /// the amount you want to change the object's rotation by each frame, and also along which axis
+    /// </summary>
+    public Vector3 newRotateAmount;
+    /// <summary>
+    /// how long you want it to take to move between point A and point B
+    /// </summary>
+    public float moveTime = 3;
+    /// <summary>
+    /// the amount of time the object has been in the animation
+    /// </summary>
+    public float currentTime = 0;
 
-	// Use this for initialization
-	IEnumerator Start () {
-        Vector3 pointA = transform.position;
-        while(true)
-        {
-            yield return StartCoroutine(MoveObject(transform, pointA, pointB, 3.0f));
-            yield return StartCoroutine(MoveObject(transform, pointB, pointA, 3.0f));
-
-        }
-
+    
+	void Start () {
+        pointA = transform.position;
     }
 
-    IEnumerator MoveObject(Transform thisTransform, Vector3 startPos, Vector3 endPos, float time) {
-        float i = 0.0f;
-        float rate = 1.0f / time;
-        while (i < 1.0f)
-        {
-            i += Time.deltaTime * rate;
-            thisTransform.position = Vector3.Lerp(startPos, endPos, i);
-            yield return null;
-        }
-	}
-    /*
-    IEnumerator RotateObject(Transform thisTransform, Vector3 startRot, Vector3 newRot)
+    void Update()
     {
-        float i = 0.0f;
-        newRot =(startRot += incritement);
-        while (i < 1.0f)
+        if (isTransform)
         {
-            i += Time.deltaTime;
-            thisTransform.rotation.x = newRot.x;
-            yield return null;
+            ObjectMove(transform);
+        }
+        if (isSpinner)
+        {
+            transform.Rotate(newRotateAmount * Time.deltaTime);
         }
     }
-    */
+
+    /// <summary>
+    /// lerp the object's tranform between point A and B
+    /// </summary>
+    /// <param name="thisTransform"></param>
+    private void ObjectMove(Transform thisTransform)
+    {
+        currentTime += Time.deltaTime;
+        float percent = currentTime / moveTime;
+        if (fromFirst)
+          {
+             thisTransform.position = Vector3.Lerp(pointA, pointB, percent);
+          }
+        if (!fromFirst)
+            {
+                thisTransform.position = Vector3.Lerp(pointB, pointA, percent);
+            }
+        if (percent >= 1)
+        {
+            fromFirst = !fromFirst;
+            currentTime = 0;
+        }
+    }
+   
+    
 }
