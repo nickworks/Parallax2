1c5acb2622c631d0111fec134cbf9eda3bf7a4ed
Dominic <djmodd@live.com>
2018-11-29 11:16:10 -0500

    Add Bulk Editing Features.cs
    They don't work yet but the script is in place

---
+++ Assets/Scripts/BulkEditingFeatures.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BulkEditingFeatures : MonoBehaviour {
+
+
+
+    public bool snapAllLayersInEditor = false;
+
+    bool snapAllLayersPrev = false;
+
+    public bool turnBlockingVolumesOff = false;
+
+    bool turnOffBlockingVolumesPrev = false;
+
+
+
+
+    private void OnValidate() {
+        if (snapAllLayersInEditor && !snapAllLayersPrev) {
+            //set the snapToLayerInEditor variable on all objects with the layer frixed component to true
+
+        } else if (!snapAllLayersInEditor && snapAllLayersPrev) {
+            //set the snapToLayerInEditor variable on all objects with the layer frixed component to false
+        }
+
+        if (turnBlockingVolumesOff && !turnOffBlockingVolumesPrev) {
+            //setactive as false for all objects with the blocking volume tag
+
+        } else if(!turnBlockingVolumesOff && turnOffBlockingVolumesPrev) {
+            //setactive as true for all objects with the blocking volume tag
+        }
+
+        snapAllLayersPrev = snapAllLayersInEditor;
+
+        turnOffBlockingVolumesPrev = turnBlockingVolumesOff;
+    }
+
+    
+}
