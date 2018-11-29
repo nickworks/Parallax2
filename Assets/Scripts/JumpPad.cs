fbc214f7cc8356ed11cf4924522c663655a97ebc
Jessica <petroej1@ferris.edu>
2018-11-28 22:45:42 -0500

    Jump Pads finally work!

--- Assets/Scripts/JumpPad.cs
+++ Assets/Scripts/JumpPad.cs
@@ -1,24 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 /// <summary>
 /// Creates a surface that autojumps the player
 /// </summary>
 public class JumpPad : MonoBehaviour {
-	/// <summary>
-	/// The amount of force behing the jump. Currently set in engine
-	/// </summary>
-	public float amountOfForce;
-	/// <summary>
-	/// Raises the trigger enter event.
-	/// </summary>
-	/// <param name="other">The other object the player collides with</param>
-	void OnTriggerEnter(Collider other){
+    private PlayerController playerController;
+    /// <summary>
+    /// The amount of force behing the jump. Currently set in engine
+    /// </summary>
+    public float amountOfForce;
+    /// <summary>
+    /// Raises the trigger enter event.
+    /// </summary>
+    /// <param name="other">The other object the player collides with</param>
+    private void Start()
+    {
+        playerController = GetComponent<PlayerController>();
+    }
+    void OnTriggerEnter(Collider other){
+
 		if(other.gameObject.CompareTag("Jumpable")){			
 			Vector3 playerPos = this.transform.position;
 			playerPos.y += amountOfForce;
 			transform.position = playerPos;
-			print (transform.position.y);
+            print(playerController.velocity);
+            playerController.velocity = Vector3.zero; 
+            playerPos *= .5f;
 			}
 	}
 }
