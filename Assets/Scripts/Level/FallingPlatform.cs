f307676e0a54a26e15c0e0b93ac2c346eb3f8349
Jessi <petroej1@ferris.edu>
2018-12-06 11:20:51 -0500

    Creating falling platforms
    They work except they player falls through them because they're a trigger. Finding a workaround.

---
+++ Assets/Scripts/Level/FallingPlatform.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlatform : MonoBehaviour {
+
+	// Use this for initialization
+	private Rigidbody rb;
+	public float fallDelay = 1f;
+	int speed = 5;
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		print ("Collided");
+		if (other.tag == "Player")
+		{
+			print ("Colluded with player");
+			Invoke ("Fall", fallDelay);
+		}
+	}
+
+	void Fall()
+	{
+		print ("I'm using gravity!");
+		rb.useGravity = true;
+	}
+}
+	
