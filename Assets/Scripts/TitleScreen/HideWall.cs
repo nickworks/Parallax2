dab11908a761d8461b1f93db0b5f618f269d2791
Blitz <brown.r1992@gmail.com>
2018-12-06 15:17:44 -0500

    Started Updating Title Screen
    Switching UI from buttons to trigger areas

---
+++ Assets/Scripts/TitleScreen/HideWall.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideWall : MonoBehaviour {
+
+    public GameObject wall;
+
+	void OnTriggerEnter (Collider other)
+    {
+        Debug.Log("sup");
+
+        wall.SetActive(false);
+    }
+}
